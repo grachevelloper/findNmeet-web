c02f2dc17f0cd4781057f609b05b906a7048b532
Nick Grachev <harizma.yt.2006@gmail.com>
2026-04-14 12:54:13 +0300

    feat: wire AppProviders into entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/styles/index.css'
-import App from './App.tsx'
+import { AppProviders } from './app/providers'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProviders />
   </StrictMode>,
 )
