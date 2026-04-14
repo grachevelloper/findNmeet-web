fed4f2968c2395540f32deab6e4a476bcaa161d6
Nick Grachev <harizma.yt.2006@gmail.com>
2026-04-14 12:53:45 +0300

    feat: setup AppProviders (antd, react-query, react-router)

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ConfigProvider } from 'antd'
+import { RouterProvider } from 'react-router'
+import { router } from '../router'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+})
+
+export function AppProviders() {
+  return (
+    <ConfigProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
+    </ConfigProvider>
+  )
+}
